1d62f32f28f53ccfba594e60e4f8e91730af4896
lucasvil4r <lucasvilar-celestino@hotmail.com>
2022-05-06 15:21:18 -0300

    Update

--- Scraping_produtosCAT6_CAT6A.py
+++ Scraping_produtosCAT6_CAT6A.py
@@ -1,12 +1,28 @@
 import urllib.request
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
+#def obterCodigoFonte(url):
+#    chrome_options = webdriver.ChromeOptions()
+#    chrome_options.add_argument('--headless')
+#    driver = webdriver.Chrome(executable_path=r'C:\Users\DEPOSITO\Clone repositorio\Web Scraping\chromedriver_win32\chromedriver.exe', chrome_options=chrome_options)
+#    driver.get(url)
+#    return driver.page_source
 
+#def processarCodigoFonte(cf):
+#    soup = BeautifulSoup(cf, 'html.parser')
+#    getValueFromDiv = soup.find('p', class_='product-name title break-all')
+#    return getValueFromDiv.text
+#
+#url = 'https://www.furukawalatam.com/pt-br/catalogo-de-produtos-categoria/FCS/'
+#codigoFonte = obterCodigoFonte(url)
+#print(processarCodigoFonte(codigoFonte))
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+driver = webdriver.Chrome(executable_path=r'C:\Users\DEPOSITO\Clone repositorio\Web Scraping\chromedriver_win32\chromedriver.exe', chrome_options=chrome_options)
+driver.get
 url = 'https://www.furukawalatam.com/pt-br/catalogo-de-produtos-categoria/FCS/'
 page = urllib.request.urlopen(url)
 soup = BeautifulSoup(page, 'html.parser')
-list_prod = soup.find_all('p', attrs={'class': 'product-name title break-all'})
-print(soup.prettify())
-
-#for prod in list_prod:
-#    print(prod.get_text())
+print(soup.prettify)
--- Scraping_todosProdutos.py
+++ Scraping_todosProdutos.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 url = 'https://www.efurukawa.com/br/c?Nrpp=437&ajaxPage=false'
 page = urllib.request.urlopen(url)
-soup = BeautifulSoup(page, 'html5lib')
+soup = BeautifulSoup(page, 'html.parser')
 list_prod = soup.find_all('h2', attrs={'class': 'product-name'})
 list_code = soup.find_all('span', attrs={'product-code'})
 
