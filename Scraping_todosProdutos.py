898bfbffc1ed5cb20bd145ba77b9e86aea26e1f4
lucasvil4r <90861563+lucasvil4r@users.noreply.github.com>
2022-05-07 11:57:40 -0300

    Update

--- Scraping_todosProdutos.py
+++ Scraping_todosProdutos.py
@@ -4,12 +4,11 @@ from bs4 import BeautifulSoup
 url = 'https://www.efurukawa.com/br/c?Nrpp=437&ajaxPage=false'
 page = urllib.request.urlopen(url)
 soup = BeautifulSoup(page, 'html.parser')
-list_prod = soup.find_all('h2', attrs={'class': 'product-name'})
+list_prod = soup.find_all('h2', attrs={'product-name'})
 list_code = soup.find_all('span', attrs={'product-code'})
 
 for prod in list_prod:
-    prodText = prod.get_text()
-    print(prodText)
+    print(prod.get_text())
+
 for code in list_code:
-    codeText = code.get_text()
-    print(codeText)
+    print(code.get_text())
