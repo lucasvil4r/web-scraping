7bed6c0c6430d106b30325a8fbbbcd278ef593ed
lucasvil4r <lucasvilar-celestino@hotmail.com>
2022-06-03 15:10:53 -0300

    Update

---
+++ Protocolo HTTP/chromedriver_win32/FCS Furukawa Connectivity System.py
@@ -0,0 +1,41 @@
+import time
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.firefox.options import  Options
+import json
+import urllib.request
+
+cont = 101
+
+while cont != 292:
+    cont +=1
+    
+    url = (f'https://www.furukawalatam.com/pt-br/catalogo-de-produtos-categoria/FCS/?page={cont}')
+
+# Pegar conteudo HTML a partir da URL
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    driver = webdriver.Chrome(executable_path=r'C:\xampp\htdocs\diretorio\Web-Scraping\Protocolo HTTP\chromedriver_win32\chromedriver.exe')
+    driver.get(url)
+
+    time.sleep(25)
+
+    element = driver.find_element_by_xpath("//div[@class='container d-flex mt-2']")
+    #driver.find_element_by_xpath("//nav[@class='d-flex justify-content-center mt-5']//ul[@class='pagination']//li[@class='page-item']//a[class='page-link active']").click()
+    html_content = element.get_attribute("outerHTML")
+
+
+    #Parsear o conteudo HTML  - BeautifulSoup
+
+    soup = BeautifulSoup(html_content, "html.parser")
+    descricao = soup.find_all('p', attrs={'product-name title break-all'})
+
+    #for prod in descricao:
+    #    descricao = prod.get_text()
+    #    print(descricao)   
+
+    driver.quit()
+    
